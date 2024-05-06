a22613ce91771fa7d4cbda1eace3cd57612b0627
notosgnikusnsi <notosgnikusnsi@gmail.com>
2024-05-06 20:19:35 +0900

    feat: Add Typing Game homepage

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,57 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: "Typing Game",
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+        useMaterial3: true,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: HomePageBody(),
+    );
+  }
+}
+
+class HomePageBody extends StatelessWidget {
+  const HomePageBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "The quick brown fox jumps over the lazy dog.",
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            TextField(),
+            TextButton(
+              onPressed: null,
+              child: Text("Start"),
+            ),
+            Text(
+              "time: 00:00",
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
         ),
       ),
     );
