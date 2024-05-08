5f511eb6b4249084e97cf2b80ae3764c759d673a
notosgnikusnsi <notosgnikusnsi@gmail.com>
2024-05-08 09:34:03 +0900

    feat: Implement dynamic word display in Typing Game homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,6 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Typing Game",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
@@ -33,10 +36,41 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class HomePageBody extends StatelessWidget {
+class HomePageBody extends StatefulWidget {
   const HomePageBody({
     super.key,
   });
+
+  @override
+  State<HomePageBody> createState() => _HomePageBodyState();
+}
+
+class _HomePageBodyState extends State<HomePageBody> {
+  List<String> _typingSentences = [
+    "The quick brown fox jumps over the lazy dog.",
+    "A journey of a thousand miles begins with a single step.",
+    "In the midst of winter, I found there was, within me, an invincible summer.",
+    "Not all those who wander are lost.",
+    "Life is what happens when you're busy making other plans.",
+    "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+    "All our dreams can come true, if we have the courage to pursue them.",
+    "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    "The only limit to our realization of tomorrow will be our doubts of today.",
+    "You must be the change you wish to see in the world."
+  ];
+  List _words = [];
+  String _showWords = "";
+  int _wordsIndex = 0;
+
+  void _start() {
+    int _typingSentenceIndex = 0;
+    String _typingSentence = _typingSentences[_typingSentenceIndex];
+    _words = _typingSentence.split(" ");
+
+    setState(() {
+      _showWords = _words.map((word) => word).join(" ");
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,12 +81,12 @@ class HomePageBody extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             Text(
-              "The quick brown fox jumps over the lazy dog.",
+              "$_showWords",
               style: Theme.of(context).textTheme.headlineLarge,
             ),
             TextField(),
             TextButton(
-              onPressed: null,
+              onPressed: _start,
               child: Text("Start"),
             ),
             Text(
